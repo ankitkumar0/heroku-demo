a400e157bfc7e1735ce4cd27739c57193ed419a9
ankit kumar <55060673+ankitkumar0@users.noreply.github.com>
2021-05-15 18:19:55 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 15 17:40:07 2021
+
+@author: Deepak Gupta
+"""
+
---
+++ model.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 15 17:06:54 2021
+
+@author: Deepak Gupta
+"""
+
